4432feb475ff45161e7dc259bb75d021b255a03d
Aryan Narayan <aryanshrivastava788@gmail.com>
2025-05-06 00:46:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  const steps = [
+    "Create React Application",
+    "Set up GitHub Repository",
+    "Install Jenkins",
+    "Configure Jenkins Pipeline",
+    "Set up Webhooks",
+    "Deploy Application"
+  ]
+
+  return (
+    <div className="container">
+      <h1>Welcome to CI/CD Pipeline with Jenkins</h1>
+      <div className="steps-container">
+        <h2>Implementation Steps:</h2>
+        <ul>
+          {steps.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
